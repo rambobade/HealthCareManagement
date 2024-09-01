91f02cc7bb59976119215f5b24b4ca89000b2115
Ram Bobade <rambobade15@gmail.com>
2024-09-01 21:21:56 +0530

    first commit

---
+++ src/main/java/com/example/healthcaremanagement/LoginPage.java
@@ -0,0 +1,23 @@
+package com.example.healthcaremanagement;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+
+public class LoginPage {
+    @FXML
+    private TextField login;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    void LoginButton(ActionEvent event) {
+
+    }
+}
